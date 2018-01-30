2198d65dbc59ae4b6b25e32e51463183bf228d16
Nouran Nouh <nourannouh@cmail.carleton.ca>
2018-01-30 11:14:59 -0500

    Add files via upload
    simple python code that asks user to enter any number and print a message that tells weather it is even or odd number

---
+++ odd_or_even.py
@@ -0,0 +1,10 @@
+#python code to ask user to enter any number as input 
+#and output a message that tells weather this number is even or odd
+number= int(raw_input ("Please Enter a Number :"))
+if number%2==0:
+    print ("this number is even")
+else :
+	print ("this number is odd")
+
+
+
