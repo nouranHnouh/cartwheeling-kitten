bbcfbae42e4729346e62e92c39c77f371388fec9
Nouran Nouh <nourannouh@cmail.carleton.ca>
2018-02-09 21:03:59 -0500

    Update table.py

--- table.py
+++ table.py
@@ -1,6 +1,4 @@
-# We now would like to summarize this data and make it more visually appealing
-# We want to go through count_list and print a table that shows the number and its 
-# corresponding count.
+#table the represent number and its occurance
 
 # The output should look like this neatly formatted table:
 """
@@ -17,19 +15,19 @@ number | occurrence
      9 | 1
     10 | 2
 """
-# Here is our code we have written so far:
+
 
 import random
 
 # Create random list of integers using while loop --------------------
 random_list = []
 list_length = 20
-
+#add random number to the list 
 while len(random_list) < list_length:
     random_list.append(random.randint(0,10))
 
 # Aggregate the data -------------------------------------------------
-count_list = [0] * 11
+count_list = [0] * 11 #create list of zeros
 index = 0
 
 while index < len(random_list):
@@ -55,24 +53,19 @@ number | occurrence
     10 | 2
 """
 
-# Hint: To print 10 blank spaces in a row, we can multiply a string by a number "n"
-# to print this string n number of times:
+#print the table 
 index=0
 print "number|occurance"
 num_len=len("number")
 
 while index<len(count_list):
-    num_spaces=num_len-len(str(index))
+    num_spaces=num_len-len(str(index)) # calculate the number of spaces that each number takes
     
-    #print " "*num_spaces+str(index)+'|'+(count_list[index])*"*"
+    #print " "*num_spaces+str(index)+'|'+(count_list[index])*"*" #print histogram table below
     print " "*num_spaces+str(index)+'|'+str(count_list[index])
     index+=1
 
-# BONUS!
-# From your summarize code you just wrote, can you make the table even more visual by 
-# replacing the count with a string of asterisks that represent the count of a number?
-# The table should look like
-
+#histogram
 """
 number | occurrence
      0 | *
@@ -88,5 +81,3 @@ number | occurrence
     10 | **
 """
 
-# Congratulations! You just created a distribution table of a list of numbers! 
-# This is also known as a histogram
