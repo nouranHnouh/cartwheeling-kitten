29ea315bd88fc1d90f40f6c9d337a789d55a7139
Nouran Nouh <nourannouh@cmail.carleton.ca>
2018-02-09 20:16:41 -0500

    Delete Days_between_Dates.py

--- Days_between_Dates.py
+++
@@ -1,9 +0,0 @@
-daysOfMonths=[31,28,31,30,31,30,31,31,30,31,30,31]
-def isLeapYear(year):
-	if year%4==0 or year%400==0:
-		return True 
-	elif year %100==0:
-		return False
-	else:
-		return False
-		
