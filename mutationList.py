9a943ead59f3a1940fe62dead4d01b78bb9a28d1
Nouran Nouh <nourannouh@cmail.carleton.ca>
2018-02-09 20:56:22 -0500

    Update mutationList.py

--- mutationList.py
+++ mutationList.py
@@ -1,17 +1,8 @@
-# We defined:
+# define a list 
 
 stooges = ['Moe','Larry','Curly']
 
-# but in some Stooges films, Curly was
-# replaced by Shemp.
-
-# Write one line of code that changes
-# the value of stooges to be:
-
-#['Moe','Larry','Shemp']
-
-# but does not create a new List
-# object.
+#print the list 
 print stooges
-stooges[2]='Shemp'
-print stooges
+stooges[2]='Shemp' # mutate the index 2 
+print stooges # pprint the list after mutation
