492a921566a7a34e26158585b0c5c6f05e8ec92d
Nouran Nouh <nourannouh@cmail.carleton.ca>
2018-02-09 20:29:58 -0500

    Update aliasingList.py

--- aliasingList.py
+++ aliasingList.py
@@ -1,7 +1,11 @@
+#create spy list 
 spy=[0,0,7]
+#assign list to a variable
 agent=spy
-print spy
-print agent
+print spy # print spy list 
+print agent # print agent list, it should be = to spy since it is pointing to the same list
+#add 1 to 7 in agent list and save it in spy list 
 spy[2]=agent[2]+1
-print spy
+#print new list
+print spy 
 print agent 
