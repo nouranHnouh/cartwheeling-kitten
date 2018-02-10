2c663e7854b930febdecf240c24bc83c2eaf8397
Nouran Nouh <nourannouh@cmail.carleton.ca>
2018-02-09 20:40:48 -0500

    Update countdown.py

--- countdown.py
+++ countdown.py
@@ -2,11 +2,7 @@
 # positive whole number as its input, and prints
 # out a countdown from that number to 1,
 # followed by Blastoff!
-# The procedure should not return anything.
-# For this question, you just need to call 
-# the procedure using the line
-# countdown(3)
-# instead of print countdown(3).
+
 def countdown(n):
     if n>0:
         i=n
