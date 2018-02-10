fc81f7816c266fa45125bd8e5522c5650a4840f6
Nouran Nouh <nourannouh@cmail.carleton.ca>
2018-02-09 20:53:28 -0500

    Update isLeapYear.py

--- isLeapYear.py
+++ isLeapYear.py
@@ -1,3 +1,5 @@
+#procedure is_leap takes one input and return true if it is leap year
+#otherwise, return false 
 def is_leap(year):
     
     if year%4==0:
@@ -8,13 +10,7 @@ def is_leap(year):
     	leap=True
     else:
     	return False 
-
-  
-        
-    
-    
-    
-  
     return leap
+#ask user for a year 
 year=int(raw_input("Enter year :"))
-print is_leap(year)
+print is_leap(year) # print whether the year is leap year or not 
